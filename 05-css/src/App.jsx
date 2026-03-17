48e9c5ad3c120dc46befba3272cd53cf0c331fc0
Bittu kumar <bittukum59@gmail.com>
2026-03-17 22:36:50 +0530

    05-css module

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from './components/button/Button'
+import Header from './components/header/Header'
+
+function App() {
+  return (
+    <div>
+      <Button />
+      <Header />
+    </div> 
+  )
+}
+
+export default App
---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1 className={styles.title}>bittu</h1>
+      <button className={styles.button}>Login</button>
+    </div>
+  )
+}
+
+export default Header
